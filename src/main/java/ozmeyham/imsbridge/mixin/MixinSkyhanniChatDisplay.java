ac5c0ead51650d333785a92dec00e181449f4c17
nopo <nopotheemail@gmail.com>
2026-01-09 23:44:52 +1100

    no remap

--- src/main/java/ozmeyham/imsbridge/mixin/MixinSkyhanniChatDisplay.java
+++ src/main/java/ozmeyham/imsbridge/mixin/MixinSkyhanniChatDisplay.java
@@ -10,7 +10,7 @@ import ozmeyham.imsbridge.IMSBridge;
 @Pseudo
 @Mixin(targets = "at.hannibal2.skyhanni.features.chat.CurrentChatDisplay")
 public class MixinSkyhanniChatDisplay {
-	@Inject(at = @At("HEAD"), method = "drawDisplay", cancellable = true)
+	@Inject(at = @At("HEAD"), method = "drawDisplay", cancellable = true, remap = false)
 	private void init(CallbackInfoReturnable<String> cir) {
 		if (IMSBridge.combinedBridgeChatEnabled) cir.setReturnValue("§aChat: §6CBridge");
 	}
